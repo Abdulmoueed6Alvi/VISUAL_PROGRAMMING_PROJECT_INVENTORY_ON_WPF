e1c6a64cb100d1be1c78648ef3bb52bc4f2ce7ef
Abdulmoueed6Alvi <abdulmoueedalvi4456@gmail.com>
2024-12-31 21:16:29 +0500

    sd

---
+++ VP_PROJECT_INVENTORY_WPF/INVENTORY_MANAGMENT_SYSTEM_233536_233532_233590/INVENTORY_MANAGMENT_SYSTEM_233536_233532_233590/category.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace INVENTORY_MANAGMENT_SYSTEM_233536_233532_233590
+{
+    public partial class Category : Window
+    {
+        public Category()
+        {
+            InitializeComponent();
+        }
+    }
+}
